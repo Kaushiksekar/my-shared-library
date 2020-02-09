7b8f2c619687f1cdd25c4c4fce4b01a1917147fe
Kaushiksekar <kaushik3993@gmail.com>
2020-02-10 01:58:59 +0530

    First commit

---
+++ src/com/trial/jenkins/main/Application.groovy
@@ -0,0 +1,10 @@
+package com.trial.jenkins.main
+
+import com.trial.jenkins.stage.Stage
+
+class Application {
+
+    public static void main(String[] args) {
+        println(Stage.getName("Stage 1"));
+    }
+}
---
+++ src/com/trial/jenkins/stage/Stage.groovy
@@ -0,0 +1,22 @@
+package com.trial.jenkins.stage
+
+class Stage {
+
+    static getName(String name1){
+        return "The name is ${name1}";
+    }
+
+//    private String name;
+//
+//    Stage(String name) {
+//        this.name = name
+//    }
+//
+//    String getName() {
+//        return name
+//    }
+//
+//    void setName(String name) {
+//        this.name = name
+//    }
+}
